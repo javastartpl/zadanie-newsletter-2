af9ed6e8785f95df7739e94b5026b6afe0287532
Zdzisław Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2019-03-22 11:31:42 +0100

    correction

--- .idea/src/Exercise_2.java
+++ .idea/src/Exercise_2.java
@@ -1,67 +1,63 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Exercise_2 {
-    public static void main(String[] args){
-        if(args.length==0) {
+    public static void main(String[] args) {
+        checkArgs(args);
+        int[] sumOfPair = getSumsOfPair(args);
+        int maxValue = Arrays.stream(sumOfPair).max().getAsInt();
+        int minValue = Arrays.stream(sumOfPair).min().getAsInt();
+
+        String maxPrefix = "Najwieksza para";
+        String minPrefix = "Najmniejsza para";
+        if (countOccurance(maxValue, sumOfPair) >= 2) maxPrefix = "Największe pary";
+        if (countOccurance(minValue, sumOfPair) >= 2) minPrefix = "Najmniejsze pary";
+
+        StringBuilder maxMessage = new StringBuilder(maxPrefix);
+        StringBuilder minMessage = new StringBuilder(minPrefix);
+        for (int i = 0; i < args.length - 1; i++) {
+            if ((Integer.valueOf(args[i]) + Integer.valueOf(args[i + 1])) == minValue && (!minMessage.toString().contains(args[i] + ", " + args[i + 1]))) {
+                buildMessage(args, minMessage, i);
+            }
+            if ((Integer.valueOf(args[i]) + Integer.valueOf(args[i + 1])) == maxValue && (!maxMessage.toString().contains(args[i] + ", " + args[i + 1]))) {
+                buildMessage(args, maxMessage, i);
+            }
+        }
+        showResults(maxMessage, minMessage);
+
+    }
+
+    private static void buildMessage(String[] args, StringBuilder minMessage, int i) {
+        minMessage.append("[" + args[i] + ", " + args[i + 1] + "]");
+    }
+
+    private static void showResults(StringBuilder maxMessage, StringBuilder minMessage) {
+        System.out.println(minMessage.toString());
+        System.out.println(maxMessage.toString());
+    }
+
+    private static int[] getSumsOfPair(String[] args) {
+        int[] sumOfPair = new int[args.length - 1];
+        for (int i = 0; i < args.length - 1; i++) {
+            sumOfPair[i] = Integer.valueOf(args[i]) + Integer.valueOf(args[i + 1]);
+        }
+        return sumOfPair;
+    }
+
+    private static void checkArgs(String[] args) {
+        if (args.length == 0) {
             System.out.println("Nie podano argumentow przy starcie programu");
             System.exit(0);
         }
-        //deklaracja zmiennych tj. tablica prechowujaca sume sasiednich liczb, maxymalna i minimalna wartosc tablicy oraz 2 obiekty String
-        int []sumOfPairs=new int[args.length-1];
-        int maxValue;
-        int minValue;
-        String maxPrefix="Najwieksza para";
-        String minPrefix="Najmniejsza para";
+    }
 
-        // stworzenie tablicy przechowujacej sume sasiednich liczb
-        for (int i = 0; i < args.length -1; i++) {
-            sumOfPairs[i]=Integer.valueOf(args[i])+Integer.valueOf(args[i+1]);
+    private static int countOccurance(int value, int[] sumOfPair) {
+        int count = 0;
+        for (int i = 0; i < sumOfPair.length - 1; i++) {
+            if (value == sumOfPair[i]) count++;
         }
-        // przypisanie pierwszej wartosci z tablicy do zmiennych
-        maxValue=sumOfPairs[0];
-        minValue=sumOfPairs[0];
-
-        // iteracja po stworzonej tablicy
-        for (int i = 1; i <sumOfPairs.length ; i++) {
-            //szukanie wartosci maksymalnej
-            if(sumOfPairs[i]>maxValue){
-                maxValue=sumOfPairs[i];
-                maxPrefix="Najwieksza para";
-            }
-            // sprawdzenie czy jest ich wiecej niz 1
-            else if (sumOfPairs[i]==maxValue) {
-                maxPrefix="Najwieksze pary";
-            }
-            // szukanie wartosci minimalnej
-            if (sumOfPairs[i]<minValue){
-                minValue=sumOfPairs[i];
-                minPrefix="Najmniejsza para";
-            }
-            // sprawdzenie cz jest ich wiecej niz 1
-            else if(sumOfPairs[i]==minValue){
-                minPrefix="Najmniejsze pary";
-            }
-        }
-
-        // utworzenie dwoch obiektow StringBuilder
-        StringBuilder maxMessage=new StringBuilder(maxPrefix);
-        StringBuilder minMessage=new StringBuilder(minPrefix);
-
-        // iteracja po tablicy, ktora zostala przekazana jako argument programu
-        for (int i = 0; i <args.length-1 ; i++) {
-            // sprawdzenie, czy sasiadujace liczby sa rowne wartosci minimalnej oraz czy nie zostaly juz dodane do obiektu minMessage
-            if((Integer.valueOf(args[i])+Integer.valueOf(args[i+1]))==minValue && (!minMessage.toString().contains(args[i] + ", " + args[i+1]))) {
-                minMessage.append("[" + args[i] + ", " + args[i+1] + "]");
-            }
-            //sprawdzenie, czy sasiadujace liczby sa rowne wartosci maksymalnej oraz czy nie zostaly juz dodane do obiektu maxMessage
-            if((Integer.valueOf(args[i])+Integer.valueOf(args[i+1]))==maxValue && (!maxMessage.toString().contains(args[i] + ", " + args[i+1]))) {
-                maxMessage.append("[" + args[i] + ", " + args[i+1] + "]");
-            }
-
-
-        }
-        // wyswietlenie wynikow
-        System.out.println(minMessage.toString());
-        System.out.println(maxMessage.toString());
-
+        return count;
     }
 
 
